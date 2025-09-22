c1448d34787f1fb8bfbcb93852c250702ca8e2cc
shakiel17 <hisoka.aboy@gmail.com>
2025-09-22 12:36:05 +0800

    Update navbar.php

--- application/views/templates/navbar.php
+++ application/views/templates/navbar.php
@@ -58,7 +58,7 @@
                                 <a href="404.html" class="dropdown-item">404 Page</a>
                             </div>
                         </div> -->
-                        <a href="contact.html" class="nav-item nav-link">Contact</a>
+                        <!-- <a href="contact.html" class="nav-item nav-link">Contact</a> -->
                     </div>
                     <!-- <a href="" class="btn btn-primary rounded-pill py-2 px-4 ms-lg-4">Book Now</a> -->
                 </div>
